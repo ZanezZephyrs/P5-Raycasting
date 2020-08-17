3cde6e0e65d270621879e27e755d905fc58e051b
ZanezZephyrs <thalesrogerio@gmail.com>
2020-08-17 15:43:03 -0300

    adicionando muros de borda no raycasting

--- src/sketch.js
+++ src/sketch.js
@@ -1,17 +1,29 @@
 let walls=[];
 let ray;
 let particle;
+
+function createBoardWalls(){
+  walls.push(new Boundary(0,0,width,0));
+  walls.push(new Boundary(width,0,width,height));
+  walls.push(new Boundary(width,height,0,height));
+  walls.push(new Boundary(0,height,0,0));
+
+}
+
 function setup() {
   createCanvas(windowWidth,windowHeight);
-  for(let i=0;i<6;i++){
+  for(let i=0;i<7;i++){
     let x1=random(width);
     let x2=random(width);
     let y1=random(height);
     let y2=random(height);
     walls.push(new Boundary(x1,y1,x2,y2));
   }
+  createBoardWalls();
   particle = new Particle();
 }
+
+
 
 function draw() {
   background(0);
