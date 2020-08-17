16ac8059e7ffb2059ab8b29ae85d875a42dd9d92
ZanezZephyrs <thalesrogerio@gmail.com>
2020-08-17 15:14:57 -0300

    adicionando boundary.js, arquivo de abstracao de obstaculos

---
+++ src/boundary.js
@@ -0,0 +1,11 @@
+class Boundary{
+    constructor(x1,y1,x2,y2){
+        this.p1=createVector(x1, y1);
+        this.p2=createVector(x2, y2);
+    }
+
+    show(){
+        stroke(255);
+        line(this.p1.x, this.p1.y, this.p2.x, this.p2.y);
+    }
+}
